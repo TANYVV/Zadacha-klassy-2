cb532daf7c1f0ea190832aecb1a00b1518a04519
TanyVV <tanusha5113@gmail.com>
2023-09-12 20:10:34 +0000

    Stack \

---
+++ Stack/Stack.cpp
@@ -0,0 +1,34 @@
+#include "Stack.hpp"
+#include <memory>
+
+class Stack
+{
+  class Node
+  {
+    int m_value;
+    std::unique_ptr<Node> next;
+
+   public:
+   
+    Node(int value, std::unique_ptr<Node> next = nullptr);
+    
+    int getValue();
+
+    void setValue(int value);
+  };
+  
+  std::unique_ptr<Node> m_node;
+
+  public:
+
+  Stack();
+
+  void push(int value);
+
+  int pop();
+
+  friend std::ostream& operator<<(std::ostream& out, Stack& obj);
+
+};
+
+std::ostream& operator<<(std::ostream& out, Stack& obj);
--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
-#include <Numbers.hpp>
+#include <Stack.hpp>
 
 int main()
 {
-  Numbers point1(3.0, 4.0, 5.0);
-  
-  Numbers point2(3.0, 4.0, 5.0);
-  
-  if(point1 == point2)
-  {
-   std::cout<<"point1 and point2 are equal\n";
-  }
-  else
-  {
-   std::cout<<"point1 and point2 are not equal\n";
-  }
-  return 0;
+
 }
